7e52ae7eafe97ec2b908db1118bda1b60d979042
Wayne Huang <vein.huang14@gmail.com>
2020-04-25 20:13:41 -0700

    Adds Material-ui and gatsby-config
    * Allows netlify dev to pick up gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
 
 export default () => (
   <>
-    <h1>Send a message</h1>
+    <Typography variant="h1">Send a Message</Typography>
+    <Button variant="contained" color="primary">
+      Material UI
+    </Button>
   </>
 );
