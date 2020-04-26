fac290556a7c1e08e8d0a055db5bbf741073bb38
Wayne Huang <vein.huang14@gmail.com>
2020-04-26 01:14:07 -0700

    Adding form status and UI

--- src/components/form.js
+++ src/components/form.js
@@ -1,16 +1,27 @@
 import React, { useReducer } from 'react';
-import { TextField, Button } from '@material-ui/core';
+import {
+  TextField,
+  Backdrop,
+  Button,
+  Typography,
+  CircularProgress
+} from '@material-ui/core';
 
 const initialState = {
   name: '',
   email: '',
   subject: '',
-  body: ''
+  body: '',
+  status: 'ERROR'
 };
 
 const reducer = (state, action) => {
   if (action.type === 'inputChange') {
     return { ...state, [action.payload.name]: action.payload.value };
+  }
+
+  if (action.type === 'statusChange') {
+    return { ...state, status: action.payload.status };
   }
 
   return state;
@@ -37,46 +48,58 @@ const Form = () => {
     console.log(state);
   };
 
+  if (state.status === 'SUCCESS') {
+    return <Typography>Message Sent!</Typography>;
+  }
+
   return (
-    <form noValidate autoComplete="off" onSubmit={onFormSubmit}>
-      <TextField
-        id="name"
-        type="text"
-        label="Name"
-        name="name"
-        required
-        onChange={onInputChange}
-        value={state.name}
-      />
-      <TextField
-        id="email"
-        type="email"
-        label="Email"
-        name="email"
-        required
-        onChange={onInputChange}
-        value={state.email}
-      />
-      <TextField
-        id="subject"
-        type="text"
-        label="Subject"
-        name="subject"
-        onChange={onInputChange}
-        value={state.subject}
-      />
-      <TextField
-        id="body"
-        multiline
-        label="Body"
-        name="body"
-        onChange={onInputChange}
-        value={state.body}
-      />
-      <Button variant="contained" color="primary" type="submit">
-        Send
-      </Button>
-    </form>
+    <>
+      {state.status === 'ERROR' && (
+        <Typography>Something went wrong. Please try again.</Typography>
+      )}
+      <Backdrop open={state.status === 'PENDING'}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <form noValidate autoComplete="off" onSubmit={onFormSubmit}>
+        <TextField
+          id="name"
+          type="text"
+          label="Name"
+          name="name"
+          required
+          onChange={onInputChange}
+          value={state.name}
+        />
+        <TextField
+          id="email"
+          type="email"
+          label="Email"
+          name="email"
+          required
+          onChange={onInputChange}
+          value={state.email}
+        />
+        <TextField
+          id="subject"
+          type="text"
+          label="Subject"
+          name="subject"
+          onChange={onInputChange}
+          value={state.subject}
+        />
+        <TextField
+          id="body"
+          multiline
+          label="Body"
+          name="body"
+          onChange={onInputChange}
+          value={state.body}
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Send
+        </Button>
+      </form>
+    </>
   );
 };
 
